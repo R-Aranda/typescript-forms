51f744cc5195da25b75aae658e405d32007b35ee
R-Aranda <rupertaranda@gmail.com>
2022-10-11 17:31:10 -0400

    Three step form

---
+++ src/AddressForm.tsx
@@ -0,0 +1,14 @@
+export function AddressForm() {
+  return (
+    <>
+      <label>Street</label>
+      <input autoFocus required type="text" />
+      <label>City</label>
+      <input required type="text" />
+      <label>State</label>
+      <input required type="number" />
+      <label>Zip</label>
+      <input required type="number" />
+    </>
+  );
+}

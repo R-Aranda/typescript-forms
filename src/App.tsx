5fc58bc8ce76267371de8f22619f5357b4fa4b08
R-Aranda <rupertaranda@gmail.com>
2022-10-17 18:05:44 -0400

    Persisting data for all forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
+import { FormEvent, useState } from "react";
 import { AccountForm } from "./AccountForm";
 import { AddressForm } from "./AddressForm";
 import { useMultiStepForm } from "./useMultiStepForm";
 import { UserForm } from "./UserForm";
 
+type FormData = {
+  firstName: string;
+  lastName: string;
+  age: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  password: string;
+};
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+};
 function App() {
+  const [data, setData] = useState(INITIAL_DATA);
+
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+
   const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
-    useMultiStepForm([<UserForm />, <AddressForm />, <AccountForm />]);
+    useMultiStepForm([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ]);
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    alert("Successfully created account!");
+  }
+
   return (
     <div
       style={{
@@ -18,7 +61,7 @@ function App() {
         fontFamily: "Arial",
       }}
     >
-      <form action="">
+      <form onSubmit={onSubmit}>
         <div
           style={{
             position: "absolute",
@@ -42,9 +85,7 @@ function App() {
               Back
             </button>
           )}
-          <button type="submit" onClick={next}>
-            {isLastStep ? "Finish" : "Next"}
-          </button>
+          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
         </div>
       </form>
     </div>
